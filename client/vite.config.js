a8d1a89156cdffefe88bce2ca390cdaad6d77fdf
Birajit Saikia <birajitsaikia@gmail.com>
2025-01-19 00:05:21 +0530

    Fix Vite build configuration

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,14 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        secure: false,
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: './index.html'
       }
     }
+  },
+  server: {
+    port: 5173,
+    host: true
   }
 })
